023e87517734e3536814ffe0cbe90582117d43fa
DESKTOP-1TDLL25\game <gotsupuri1007@gmail.com>
2017-07-20 21:31:55 +0900

    add MouseDrag.cs and delete Aries Collider2D

---
+++ Assets/script/MouseDrag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+	Vector3 oldObjectPositon;
+
+	void OnMouseDrag()
+	{
+		oldObjectPositon = this.transform.position;
+
+		Vector3 objectPointInScreen
+		= Camera.main.WorldToScreenPoint(this.transform.position);
+
+		Vector3 mousePointInScreen
+		= new Vector3(Input.mousePosition.x,
+			Input.mousePosition.y,
+			objectPointInScreen.z);
+
+		Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+		mousePointInWorld.z = this.transform.position.z;
+		this.transform.position = mousePointInWorld;
+	}
+
+	void OnCollisionStay2D()
+	{
+		Debug.Log ("X:" + oldObjectPositon.x);
+		Debug.Log ("Y:" + oldObjectPositon.y);
+	}
+
+	void OnCollisionExit2D()
+	{
+		this.transform.position = oldObjectPositon;
+	}
+}
