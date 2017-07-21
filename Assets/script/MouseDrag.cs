1f8400db81691d436a6b29e3b5f336acf0eeba06
debugon <games0421@gmail.com>
2017-07-21 21:47:39 +0900

    ドラッグ移動対応

--- Assets/script/MouseDrag.cs
+++ Assets/script/MouseDrag.cs
@@ -4,33 +4,50 @@ using UnityEngine;
 public class MouseDrag : MonoBehaviour
 {
 	Vector3 oldObjectPositon;
+    bool collisionFlag = false;
 
-	void OnMouseDrag()
-	{
-		oldObjectPositon = this.transform.position;
+    private void Start()
+    {
+        oldObjectPositon = this.transform.position;
+    }
 
-		Vector3 objectPointInScreen
-		= Camera.main.WorldToScreenPoint(this.transform.position);
+    void OnMouseDrag()
+    {
+        Vector3 objectPointInScreen
+        = Camera.main.WorldToScreenPoint(this.transform.position);
 
-		Vector3 mousePointInScreen
-		= new Vector3(Input.mousePosition.x,
-			Input.mousePosition.y,
-			objectPointInScreen.z);
+        Vector3 mousePointInScreen
+        = new Vector3(Input.mousePosition.x,
+            Input.mousePosition.y,
+            objectPointInScreen.z);
 
-		Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
-		mousePointInWorld.z = this.transform.position.z;
-		this.transform.position = mousePointInWorld;
+        Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+        mousePointInWorld.z = this.transform.position.z;
+        this.transform.position = mousePointInWorld;
+    }
 
-	}
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        collisionFlag = true;
+    }
 
-	void OnCollisionStay2D(Collision2D coll)
+    void OnCollisionStay2D(Collision2D coll)
 	{
 		Debug.Log ("X:" + oldObjectPositon.x);
 		Debug.Log ("Y:" + oldObjectPositon.y);
-	}
+
+        oldObjectPositon = this.transform.position;
+    }
 
 	void OnCollisionExit2D(Collision2D coll)
-	{
-		this.transform.position = oldObjectPositon;
-	}
+    {
+        this.transform.position = oldObjectPositon;
+    }
+
+    private void OnMouseUp()
+    {
+        if (!collisionFlag)
+        {
+        }
+    }
 }
